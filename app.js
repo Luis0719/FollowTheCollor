0e170873557f9fccbf3b1e1c7d712705629aaa78
Luis Hernandez Ayala <luishernandezayala@MacBook-Pro-de-Luis-2.local>
2019-11-20 20:42:26 -0600

    Basic funcionality

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-var leader;
-var hasGameStarted = false;
-var readyPlayers = 0;
-var playersOnGame = 0;
-var colorSequence = [];
-var round = 1;
-var changingLeader = false;
+game = {
+  leader: null,
+  hasGameStarted: false,
+  readyPlayers: 0,
+  playersOnGame: 0,
+  colorSequence: [],
+  round: 1,
+  changingLeader: false,
+  finishedPlayers: 0
+}
 
 // Define static folder
 app.use(express.static('public'));
@@ -62,7 +65,7 @@ app.get('/leader', function(req, res){
 
 // Load game logic using socket.io
 io.on('connection', function(socket){
-  // if (hasGameStarted){
+  // if (game.hasGameStarted && !game.changingLeader){
   //   console.log("The game has already started");
   //   socket.disconnect();
   //   return;
@@ -71,67 +74,42 @@ io.on('connection', function(socket){
   console.log('connected with id: ' + socket.id);
 
   socket.on('disconnect', function () {
-    if (!hasGameStarted){
+    if (!game.hasGameStarted){
       io.sockets.emit('lobby-remove-player', socket.id);
     }
 
-    if (!changingLeader && hasGameStarted && socket.id == leader.id){
+    if (!game.changingLeader && game.hasGameStarted && socket.id == game.leader.id){
       console.log(`Leader with ID ${socket.id} disconnected. Initiating process to select new leader.`);
-      leader = get_leader();
-      console.log(`New leader: ${leader.id}`);
-      leader.emit('redirect', `http://localhost:3000/leader?name=${leader.playername}`);
-      changingLeader = true;
+      game.leader = get_leader();
+      console.log(`New leader: ${game.leader.id}`);
+      game.leader.emit('redirect', `http://localhost:3000/leader?name=${game.leader.playername}`);
+      game.changingLeader = true;
     }else{
       console.log(`${socket.id} disconnected`);
-      changingLeader = false;
+      game.changingLeader = false;
+
+      if (game.hasGameStarted){
+        game.playersOnGame--;
+      }
     }
   });
 
-  socket.on('register-player', function(playername) {
+  // Lobby functions
+  socket.on('lobby-register-player', function(playername) {
     socket.playername = playername
     console.log(`${socket.playername} registered`);
 
     io.sockets.emit('lobby-add-player', socket.id, socket.playername);
   });
 
-  socket.on('game-leader-ready', function(playername) {
-    console.log(`Setting leader with ID = ${socket.id} name = ${playername}`)
-    leader = socket;
-    leader.playername = playername;
-    leader.isLeader = true;
-    
-    playersOnGame++;
-    // Start game if all players are on the game-view
-    if(playersOnGame >= readyPlayers){
-      hasGameStarted = true;
-      console.log("Starting game");
-      console.log(parse_players());
-      leader.emit('set-players', JSON.stringify(parse_players()));
-    }
-  });
-
-  socket.on('game-player-ready', function(playername) {
-    console.log(`Player ${playername} is ready`);
-    
-    socket.playername = playername;
-    playersOnGame++;
-    // Start game if all players are on the game-view
-    if(playersOnGame == readyPlayers){
-      hasGameStarted = true;
-      console.log("Starting game");
-      console.log(parse_players());
-      leader.emit('set-players', JSON.stringify(parse_players()));
-    }
-  });
-
   socket.on('lobby-player-ready', function() {
-    readyPlayers++;
+    game.readyPlayers++;
 
     var sockets = io.sockets.sockets;
     
-    if (readyPlayers == Object.keys(sockets).length){
-      leader = get_leader();
-      leader.isLeader = true;
+    if (game.readyPlayers == Object.keys(sockets).length){
+      game.leader = get_leader();
+      game.leader.isLeader = true;
 
       for (let socketId in sockets){
         if(!sockets[socketId].isLeader)
@@ -139,21 +117,83 @@ io.on('connection', function(socket){
       }
 
       // Make sure the leader is the last one to be redirected. Otherwise the socket.on('disconnect') may cause problems
-      leader.emit('redirect', `http://localhost:3000/leader?name=${leader.playername}`);
+      game.leader.emit('redirect', `http://localhost:3000/leader?name=${game.leader.playername}`);
+
+      console.log(`${game.readyPlayers} ready players`)
     }
   });
-  
-  socket.on('leader-color-choice', function(color) {    
-    colorSequence.push(color);
-    console.log(colorSequence);
+  // End of Lobby functions
 
-    io.sockets.emit('start-player-turn', round, colorSequence)
+  // Gameplay functions
+  socket.on('ld-register', function(playername) {
+    console.log(`Setting leader with ID = ${socket.id} name = ${playername}`)
+    game.leader = socket;
+    game.leader.playername = playername;
+    game.leader.isLeader = true;
+    
+    game.playersOnGame++;
+    // Start game if all players are on the game-view
+    request_start_game();
   });
 
-  socket.on('player-add-player-color', function(color) {
-    leader.emit('add-player-color', socket.id, color);
-  })
+  socket.on('usr-register', function(playername) {
+    console.log(`Player ${playername} is ready`);
+    
+    socket.playername = playername;
+
+    game.playersOnGame++;
+    // Start game if all players are on the game-view
+    request_start_game();
+  });
+  
+  socket.on('ld-choice', function(color) {    
+    game.colorSequence.push(color);
+    console.log(`Leader chosed ${color}`);
+    console.log(`New color sequence: ${game.colorSequence}`);
+
+    game.finishedPlayers = 0;
+    io.sockets.emit('usr-start-turn', game.round);
+  });
+
+  socket.on('usr-choice', function(color, choice_number) {
+    color_class = "correct";
+    
+    // Validate user choice
+    if (color != game.colorSequence[choice_number]){
+      socket.emit('usr-wrong-choice');
+      color_class = "wrong";
+      console.log(`${socket.playername} choice #${choice_number} = ${color} was ${color_class}`);
+      socket.disconnect();
+    }else{
+      if (choice_number >= game.round-1){
+        socket.emit('usr-finished-turn');
+  
+        game.finishedPlayers++;
+        console.log(`${game.finishedPlayers} finished vs ${game.playersOnGame} total`);
+      }
+    }
+  
+    console.log(`${socket.playername} choice #${choice_number} = ${color} was ${color_class}`);
+    // Notify leader of the players choice
+    game.leader.emit('ld-user-choice', socket.id, color, color_class);
+
+    if (game.finishedPlayers == game.playersOnGame-1){
+      game.round++;
+      game.leader.emit('ld-start-turn', game.round);
+    }
+  });
+  // End of Gameplay functions
 });
+
+function request_start_game(){
+  console.log(`${game.playersOnGame} total players vs ${game.readyPlayers} ready players`)
+  if(game.playersOnGame == game.readyPlayers){
+    game.hasGameStarted = true;
+    console.log("Starting game");
+    console.log(parse_players());
+    game.leader.emit('ld-set-players', JSON.stringify(parse_players()));
+  }
+}
 
 function get_leader(){
     let leader;
@@ -190,6 +230,7 @@ function parse_players() {
 }
 
 
-http.listen(process.env.PORT || 3000, function(){
-  console.log('Server listening on *:3000');
+serv_port = process.env.PORT || 3000;
+http.listen(serv_port, function(){
+  console.log(`Server listening on *:${serv_port}`);
 });
